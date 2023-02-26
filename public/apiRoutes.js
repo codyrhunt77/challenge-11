28f9e5010c206d04446db185ab1c4a3aab3a58cc
Cody Hunt <codyrhunt77@gmail.com>
2023-02-26 15:42:13 -0500

    js

--- public/apiRoutes.js
+++ public/apiRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const notepad = require('./notepad');
-router.get('../notes' , (req, res) => {
+router.get('/notes' , (req, res) => {
     notepad
     .getNotes()
     .then((notes) => {
